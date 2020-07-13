e9bc7994bf64ae69685ba3d87e3de303b432258a
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-07-13 20:27:31 -0300

    .

--- angular-TensorFlow/src/app/app.component.ts
+++ angular-TensorFlow/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { setTheme } from 'ngx-bootstrap/utils';
-
+import sha256 from 'crypto-js/sha256';
 import * as tf from '@tensorflow/tfjs';
 
 @Component({
@@ -13,6 +13,8 @@ export class AppComponent {
   inputData: Number;
   linearModel: tf.Sequential;
   prediction: any;
+  hashdata: any;
+  hashPredict: any;
   constructor() {
     setTheme('bs3');
   }
@@ -25,9 +27,21 @@ export class AppComponent {
 
     this.linearModel = tf.sequential();
     this.linearModel.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+    //this.linearModel.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+    //this.linearModel.add(tf.layers.dense({ units: 1 }));
     this.linearModel.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-    let trainData = [3.2, 4.4, 5.5, 6.6, 7.7];
-    let trainOutput = [1.6, 2.7, 2.9, 3.19, 1.684];
+    let trainData = [];
+    let trainOutput = [];
+    for (let i = 0.0; i < 10; i++) {
+      let hash = sha256(i);
+      let decimal = parseInt(hash, 16) / 10 ** 70;
+      //console.log(decimal);
+
+      trainOutput.push(parseFloat(i.toString()));
+      trainData.push(hash);
+    }
+    console.log(trainData);
+    console.log(trainOutput);
 
     const xs = tf.tensor1d(trainData);
     const ys = tf.tensor1d(trainOutput);
@@ -36,15 +50,31 @@ export class AppComponent {
     console.log('treinado!');
   }
 
-  linearPrediction(val: number) {
-    const output = this.linearModel.predict(
-      tf.tensor2d([Number(val)], [1, 1])
-    ) as any;
+  linearPrediction(val: number, hash) {
+    console.log(val);
+    console.log(val.toString(16));
 
-    this.prediction = Array.from(output.dataSync())[0];
+    const output = this.linearModel.predict(tf.tensor2d([hash], [1, 1])) as any;
+
+    this.hashPredict = Number(Array.from(output.dataSync())[0]).toString(16);
+    /*
+    this.hashPredict = this.prediction.toString().split('.')[0];
+
+    console.log(this.hashPredict);
+    for (let i = 0; i < 70; i++) {
+      this.hashPredict = this.hashPredict + '0';
+    }
+    this.hashPredict = Number(this.hashPredict).toString(16);
+    console.log(Number(this.hashPredict).toString(16));
+    */
   }
   handleChange(data: number): void {
     this.inputData = data;
-    this.linearPrediction(data);
+    this.hashdata = sha256(data);
+    let decimal = parseInt(this.hashdata, 16);
+    console.log(decimal);
+    console.log(parseInt(decimal.toString(16), 16));
+
+    this.linearPrediction(decimal, this.hashdata);
   }
 }
